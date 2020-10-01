6ea0c16e0edf8b120a0e226f16f5a9a75cb55e39
Aleksi Heikkilä <aleksi.heikkila@metropolia.fi>
2020-10-01 13:17:14 +0300

    3.2 tehty

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/info', (req, res) => {
+    res.send("Phone book has info for " + persons.length + " people <br>" + new Date())
+  })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
