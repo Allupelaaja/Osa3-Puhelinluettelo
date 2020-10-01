82961f681ca621a04115492cd4e87c511fc6a545
Aleksi Heikkilä <aleksi.heikkila@metropolia.fi>
2020-10-01 17:15:36 +0300

    uibuild

---
+++ build/precache-manifest.412f263fca0b3c89c777150e116c7746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c19f11e061f7e4d828bb7006f923b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ccb3047a6dc86a2e026",
+    "url": "/static/css/main.50630c43.chunk.css"
+  },
+  {
+    "revision": "36b255a929c5c09379bf",
+    "url": "/static/js/2.52064027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.52064027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ccb3047a6dc86a2e026",
+    "url": "/static/js/main.0f903e48.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
